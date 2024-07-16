c48740ccc749c60b962c1566f7aa2a531db07252
gitshubhsss <shubhamranjane16@gmail.com>
2024-07-16 18:48:34 +0530

    .\pattern4.java

---
+++ whileloop/pattern4.java
@@ -0,0 +1,18 @@
+public class pattern4 {
+    public static void main(String[] args) {
+        /* ****
+           ****
+           ****
+           ****
+
+           this is the pattern that we are going to print using for loop
+         */
+
+         for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+         }
+    }
+}
